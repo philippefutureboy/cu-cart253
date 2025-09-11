9a420941c94dd82f863d85c314cdb842b9cd6562
Philippe Hebert <philippe@human.space>
2025-09-11 15:45:32 -0400

    feat: Added rectangle to delimit the road area of the landscape; used alpha to better see reference

--- instructions-challenge/js/script.js
+++ instructions-challenge/js/script.js
@@ -26,4 +26,17 @@ function draw() {
   background(0);
   // Draw the image at position (0,0) with resizing 600,800 to match canvas
   image(img, 0, 0, 600, 800);
+  drawRoad();
+  // fill(color(118, 180, 219, 50));
+}
+
+/**
+ * Function to draw road in the lower half of the canvas
+ */
+function drawRoad() {
+  push();
+  fill(color(129, 148, 145, 50));
+  noStroke();
+  rect(0, 430, 600, 400);
+  pop();
 }
