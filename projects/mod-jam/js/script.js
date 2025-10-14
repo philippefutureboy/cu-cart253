0639ee33bb1bda2891aaec68b97aee9c20ee936b
Philippe Hebert <philippe@human.space>
2025-10-14 18:03:34 -0400

    dx(projects/mod-jam): Adds mouse position print for ease of development

--- projects/mod-jam/js/script.js
+++ projects/mod-jam/js/script.js
@@ -1,7 +1,23 @@
+class MouseCursor {
+  draw() {
+    const onCanvas =
+      mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height;
+
+    if (onCanvas) {
+      fill("white");
+      textAlign(LEFT, BOTTOM);
+      text(` (${Math.floor(mouseX)}, ${Math.floor(mouseY)})`, mouseX, mouseY);
+    }
+  }
+}
+
+const mouseCursor = new MouseCursor();
+
 function setup() {
   createCanvas(600, 600);
 }
 
 function draw() {
   background(0);
+  mouseCursor.draw();
 }
