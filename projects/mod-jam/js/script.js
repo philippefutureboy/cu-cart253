0b9adca54538b1facfd71b13e6829f6ba376fa47
Philippe Hebert <philippe@human.space>
2025-10-14 18:12:06 -0400

    feat(projects/mod-jam): Draw the Frog (no arms/legs yet); done in a p5js playground, thus why commits are so close together

--- projects/mod-jam/js/script.js
+++ projects/mod-jam/js/script.js
@@ -11,7 +11,79 @@ class MouseCursor {
   }
 }
 
+class Frog {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw() {
+    push();
+    translate(this.x, this.y);
+    // rotate((frameCount / 360) * 2 * PI)
+
+    // frog helmet lens
+    fill(color(255, 255, 255, 40));
+    ellipse(0, 0, 50, 50);
+
+    // frog head
+    fill("#0f0");
+    ellipse(0, 5, 40, 40);
+
+    // left eye
+    fill("#fff");
+    ellipse(-12, -8, 10, 10);
+    fill("black");
+    ellipse(-12, -10, 3, 3);
+
+    // right eye
+    fill("#fff");
+    ellipse(12, -8, 10, 10);
+    fill("black");
+    ellipse(12, -10, 3, 3);
+
+    // helmet glass top contour
+    push();
+    stroke("white");
+    noFill();
+    arc(0, -8, 44, 35, PI, 2 * PI);
+    ellipse(0, 0, 50, 50);
+    pop();
+
+    // helmet occlusion
+    push();
+    fill("#F5F7FB");
+    beginShape();
+    vectorArc(0, -8, 45, 35, 0, PI);
+    stroke("white");
+    vectorArc(0, 0, 50, 50, PI + PI / 8, -PI / 8);
+    endShape(CLOSE);
+    pop();
+
+    // helmet glass bottom contour
+    push();
+    stroke("black");
+    noFill();
+    arc(0, -8, 44, 35, 0, PI);
+    pop();
+
+    // body
+    push();
+    fill("#F5F7FB");
+    beginShape();
+    stroke("black");
+    vectorArc(0, 30, 60, 60, -PI / 4 + 0.12, (10 / 8) * PI - 0.12);
+    vectorArc(0, 1.5, 50, 50, PI + PI / 8 - 0.95, -PI / 8 + 0.95);
+    endShape(CLOSE);
+    pop();
+
+    translate(-this.x, -this.y);
+    pop();
+  }
+}
+
 const mouseCursor = new MouseCursor();
+const frog = new Frog(300, 300);
 
 function setup() {
   createCanvas(600, 600);
@@ -19,5 +91,49 @@ function setup() {
 
 function draw() {
   background(0);
+  frog.draw();
   mouseCursor.draw();
+}
+
+/**
+ * Vector-based arc, to work with complex shapes (beginShape, endShape).
+ * Supports drawing points in clockwise/counter-clockwise order, to allow endShape to close
+ * composite shapes made out vertices.
+ *
+ * Example:
+ *   Drawing a crescent moon:
+ *   ```
+ *     push();
+ *     fill("white");
+ *     beginShape();
+ *     stroke("black");
+ *     // counter-clockwise, bottom curve
+ *     vectorArc(0, 30, 60, 60, -PI / 4 + 0.12, (10 / 8) * PI - 0.12);
+ *     // clockwise, top curve
+ *     vectorArc(0, 1.5, 50, 50, PI + PI / 8 - 0.95, -PI / 8 + 0.95);
+ *     // closes the shape
+ *     endShape(CLOSE);
+ *     pop();
+ *   ```
+ *
+ * @param {number} cx x coordinate of center of arc
+ * @param {number} cy y coordinate of center of arc
+ * @param {number} dx x diameter of arc
+ * @param {number} dy y diameter of arc
+ * @param {number} a1 start angle
+ * @param {number} a2 stop angle
+ */
+function vectorArc(cx, cy, dx, dy, a1, a2) {
+  const clockwise = a1 <= a2;
+  const rx = dx / 2,
+    ry = dy / 2;
+  for (
+    let a = a1;
+    clockwise ? a <= a2 : a >= a2;
+    a += clockwise ? 0.01 : -0.01
+  ) {
+    let px = cx + cos(a) * rx;
+    let py = cy + sin(a) * ry;
+    vertex(px, py);
+  }
 }
