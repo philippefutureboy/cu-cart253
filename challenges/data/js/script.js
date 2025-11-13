f893bd6fbdfc7d2208a63bdd4bf9cc5f345af8e3
Philippe Hebert <philippe@human.space>
2025-11-13 13:45:27 -0500

    feat(challenges/data): Initial commit for challenges/data

---
+++ challenges/data/js/script.js
@@ -0,0 +1,22 @@
+/**
+ * Data Challenge
+ * 
+ * Authors:
+ * - Philippe Hebert
+ * - Emile Bedard
+ * - Joyce Lam 
+ * 
+ * The data challenge submission - work with structured data.
+ */
+
+"use strict";
+
+
+function setup() {
+
+}
+
+
+function draw() {
+
+}
