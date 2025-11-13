dcbf1b3d2eb5d5321c895e959372951fe25891c1
Philippe Hebert <philippe@human.space>
2025-11-13 13:46:13 -0500

    feat(challenges/data): Paste in the data base script from assignment description

--- challenges/data/js/script.js
+++ challenges/data/js/script.js
@@ -11,12 +11,45 @@
 
 "use strict";
 
+let carData = undefined;
+let dinosaurData = undefined;
+let langData = undefined;
+let lang = "fr";
 
-function setup() {
+// Starts with the instruction
+let carName = "Click to generate a car name.";
+
+/**
+ * Load the car and dinosaur data
+ */
+function preload() {
 
 }
 
+/**
+ * Create the canvas
+*/
+function setup() {
+    createCanvas(600, 400);
+}
 
+/**
+ * Display the current main text (either instructions or a car)
+*/
 function draw() {
+    background(0);
+
+    push();
+    fill("pink");
+    textAlign(CENTER, CENTER);
+    textSize(32);
+    text(carName, width / 2, height / 2);
+    pop();
+}
+
+/**
+ * Generate a new car name
+ */
+function mousePressed() {
 
 }
